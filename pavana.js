6f6f47e8d518f1a2913e7300557bfd3614b737f6
AgusCDT <acastrod@ucm.es>
2022-10-22 18:21:24 +0200

    ....

--- pavana.js
+++ pavana.js
@@ -1,6 +1,6 @@
 import entities from './entities.js';
 
-export default class pavana extends Phaser.GameObjects.Sprite {
+export default class pavana extends entities {
 	constructor(scene, x, y) {
 		super(scene, x, y, 200, 'pavana'); //llamamos al constructor de Enemy con velocidad 200 e imagen 'goblin'
 		this.setDisplaySize(50,50); // asignamos el tamaño con el que queremos que se vea su imagen
