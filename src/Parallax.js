9464d4b08c0be76546650de83787fd5dede557b5
AgusCDT <acastrod@ucm.es>
2022-10-25 15:26:34 +0200

    .

--- src/Parallax.js
+++ src/Parallax.js
@@ -4,10 +4,6 @@ export default class Parallax{
 		this.relatedScene = scene;
 	}
 
-	preload(){
-		this.relatedScene.load.image('parallax', './assets/escenarios/Tierra/Carretera/Carretera.jpg');
-	}
-
 	create(){
 		this.parallax = this.relatedScene.add.tileSprite(0, 0, 3200, 600, 'parallax').setOrigin(0,0);		
 	}
--- src/scenes/Tierra.js
+++ src/scenes/Tierra.js
@@ -8,18 +8,14 @@ export default class Tierra extends Phaser.Scene {
 	}
 	
 	preload() {
-		this.Parallax.preload();
-	 	/*this.load.image('tierra', './assets/escenarios/Tierra/Carretera/Carretera.jpg');*/ 
+		this.relatedScene.load.image('parallax', './assets/escenarios/Tierra/Carretera/Carretera.jpg'); 
 	 	this.load.image('pavana', './assets/imagenes/gaviota.png');
-	 	console.log("Creada");
-	 	
+	 	console.log("Creada"); 	
 	}
 	 
 	create() {
 		this.Parallax.create();
-	 	/*this.add.image(0, 0, 'tierra').setOrigin(0, 0);*/
-	 	this.pavana = new Pavana(this, 100, 100);
-	 		
+	 	this.pavana = new Pavana(this, 100, 100);	 		
 	}
 
 	update() {
